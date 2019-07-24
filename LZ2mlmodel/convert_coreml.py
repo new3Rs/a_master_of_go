2c3452bae2ad9c1718f80633fec18422681b431f
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2019-07-24 19:11:14 +0900

    added optimize.py

--- LZ2mlmodel/convert_coreml.py
+++ LZ2mlmodel/convert_coreml.py
@@ -2,13 +2,13 @@ import sys
 import tfcoreml as tf_converter
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: python {} size target_path'.format(sys.argv[0]))
+    if len(sys.argv) < 4:
+        print('Usage: python {} source size target_path'.format(sys.argv[0]))
         sys.exit()
-    size = int(sys.argv[1])
+    size = int(sys.argv[2])
     tf_converter.convert(
-        tf_model_path='tmp/frozen_model.pb',
-        mlmodel_path = sys.argv[2],
+        tf_model_path=sys.argv[1],
+        mlmodel_path = sys.argv[3],
         output_feature_names = ['fc1:0', 'fc3:0'],
         input_name_shape_dict = {'x:0' : [1, size, size, 18]}
     )
---
+++ LZ2mlmodel/optimize.py
@@ -0,0 +1,21 @@
+import sys
+import tensorflow as tf
+import optimize_for_inference_lib
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print('Usage: python {} source destination'.format(sys.argv[0])) 
+    sys.exit()
+
+  inputGraph = tf.GraphDef()
+  with tf.gfile.Open(sys.argv[1], "rb") as f:
+    data2read = f.read()
+    inputGraph.ParseFromString(data2read)
+    
+  outputGraph = optimize_for_inference_lib.optimize_for_inference(
+                inputGraph,
+                ["x"], # an array of the input node(s)
+                ["fc1", "fc3"], # an array of output nodes
+                tf.float32.as_datatype_enum)
+
+  tf.train.write_graph(outputGraph,".",sys.argv[2],as_text=False)
